d431e04bd21347e494342aa80b7f612012f8d094
Ignacio Moya <imoyared@gmail.com>
2023-03-26 22:06:06 +0200

    added sports classifier

--- lecture1/image_classifier.py
+++ lecture1/image_classifier.py
@@ -10,16 +10,18 @@ class ImageClassifier:
         if download == True:
             [ImageDownloader(image, **kwargs).download_images() for image in image_categories]
 
-    def learn(self, dls, n_epochs=3, pretrained=resnet18, metrics=error_rate):
+    def learn(self, dls, n_epochs=3, pretrained=resnet18, metrics=error_rate, **kwargs):
         learn = vision_learner(dls, pretrained, metrics)
         learn.fine_tune(n_epochs)
         self.learn = learn
+        if 'download_model' in kwargs:
+            learn.export('model.pkl')
         return learn
     
 
 
 if __name__ == "__main__":
-    image_categories = ["footbal", "baseball"]
+    image_categories = ["football", "baseball", "basketball"]
     path_folder = "classifier_sports"
     n_epochs = 6
     bs = 32
@@ -34,5 +36,8 @@ if __name__ == "__main__":
         get_y=parent_label,
         item_tfms=[Resize(192, method='squish')]
     ).dataloaders(Path(path_folder), bs=bs)
-    learn  = ic.learn(dls=dls, n_epochs=n_epochs, pretrained=pretrained)
+    learn  = ic.learn(dls=dls, n_epochs=n_epochs, pretrained=pretrained, download_model=True)
+    print(learn.predict('sports_images/football.jpg'))
+    print(learn.predict('sports_images/baseball.jpg'))
+    print(learn.predict('sports_images/basketball.jpg'))
     
